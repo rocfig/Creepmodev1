15d669cb5a625f097f38fbe0c4d1b3ca108e370f
Shawn Shteinbah <sshteinbah@gmail.com>
2016-09-01 16:54:40 -0500

    added notification

--- app/src/main/java/com/example/shawn/creepmodev1/MainActivity.java
+++ app/src/main/java/com/example/shawn/creepmodev1/MainActivity.java
@@ -1,11 +1,17 @@
 package com.example.shawn.creepmodev1;
 
+import android.app.NotificationManager;
+import android.app.Notification;
 import android.content.Intent;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.NotificationCompat;
 import android.view.View;
 import android.widget.Toast;
+import android.widget.Button;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,9 +19,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                onButtonTap();
+                createNotification();
+            }
+        } );
     }
 
-    public void onButtonTap(View v) {
+    public void onButtonTap() {
         Toast.makeText(getApplicationContext(), "Creepmode is running. Happy Creeping!", Toast.LENGTH_LONG).show();
         startActivity(new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS));
     }
@@ -23,5 +39,16 @@ public class MainActivity extends AppCompatActivity {
         Toast myToast = Toast.makeText(getApplicationContext(), "Creepmode has been disabled.", Toast.LENGTH_LONG);
         myToast.show();
     }
+    public void createNotification () {
+        NotificationCompat.Builder nBuilder = new NotificationCompat.Builder(this);
+        nBuilder.setContentTitle("Creepmode");
+        nBuilder.setContentText("Service is Running");
+        nBuilder.setSmallIcon(R.drawable.ic_servicenotify);
 
+
+        Notification notification  = nBuilder.build();
+        NotificationManager nm = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+        nm.notify (1, notification);
+    }
 }
